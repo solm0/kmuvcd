f444e967ddedf502fe5eb615f93a122922677491
정솔미 <solmi-@kookmin.ac.kr>
2025-01-27 21:39:56 +0900

    .

--- app/(education)/curriculum/page.tsx
+++ app/(education)/curriculum/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import { fetchSgData } from '@/app/lib/fetchSgData';
+import { fetchData } from '@/app/lib/fetchData';
 
 export const metadata: Metadata = {
     title: '교육과정',
@@ -9,22 +10,48 @@ interface Post {
     id?: number;
     name?: string;
     content?: string;
+    
+    credits?: number;
+    subject?: string;
+    format?: string;
+    description?: string;
+    mandatory?: boolean;
+    grade?: number;
 }
 
 export default async function Page() {
-    const data = await fetchSgData<Post>('curriculum');
+    const data_1 = await fetchSgData<Post>('curriculum');
 
-    if (!data) {
+    if (!data_1) {
         return <p>No data available or failed to load.</p>;
     }
 
+    const data_2 = await fetchData<Post>('courses');
+
+    if (!data_2) {
+        return <p>No data available or failed to load.</p>;
+    }
+
+    
     return (
         <div>
             <h1 className='text-2xl pb-8'>교육과정</h1>
-                <div key={data.id} className='rounded-lg bg-gray-100 p-8'>
-                    <p>name:{data.name}</p>
-                    <p>content:{data.content}</p>
+            <div key={data_1.id} className='rounded-lg bg-gray-100 p-8'>
+                <p>name:{data_1.name}</p>
+                <p>content:{data_1.content}</p>
+            </div>
+            <h1 className='text-2l pb-8'>교과목</h1>
+            {data_2.map((post: Post) => (
+                <div key={post.id} className='rounded-lg bg-gray-100 p-8'>
+                    <p>name: {post.name}</p>
+                    <p>credits: {post.credits}</p>
+                    <p>subject: {post.subject}</p>
+                    <p>format: {post.format}</p>
+                    <p>description: {post.description}</p>
+                    <p>mandatory: {post.mandatory}</p>
+                    <p>grade: {post.grade}</p>
                 </div>
+            ))}
         </div>
     );
 }
