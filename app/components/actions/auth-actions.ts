66f6754e3420259e5df21ca2bce29743e088c6d9
정솔미 <solmi-@kookmin.ac.kr>
2025-02-03 20:27:16 +0900

    remove domain

--- app/components/actions/auth-actions.ts
+++ app/components/actions/auth-actions.ts
@@ -8,7 +8,6 @@ import { redirect } from 'next/navigation';
 const config = {
   maxAge: 60 * 60 * 24 * 7, // 1 week
   path: "/",
-  domain: process.env.HOST ?? "localhost",
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
 };
