f43f18360a33a3cfabee122622c13a32fb1eb75b
정솔미 <solmi-@kookmin.ac.kr>
2025-02-14 11:14:32 +0900

    middleware matcher 내프로필로 한정

--- middleware.ts
+++ middleware.ts
@@ -17,17 +17,12 @@ export async function middleware(request: NextRequest) {
   const currentPath = request.nextUrl.pathname;
 
   if (isProtectedRoute(currentPath)) {
-    console.time("Middleware Execution Time");
-
     const user = await getUserMeSmall();
-    console.log("User Status: ", user.ok);
 
     if (user.ok === false) {
       console.log("Redirecting to signin...");
       return NextResponse.redirect(new URL("/signin", request.url));
     }
-
-    console.timeEnd("Middleware Execution Time");
   }
 
   return NextResponse.next();
@@ -35,13 +30,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/myprofile"
   ],
 };
