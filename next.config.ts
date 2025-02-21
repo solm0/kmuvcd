28f64edb3047b64cfaa09be764656fc5d4fec648
정솔미 <solmi-@kookmin.ac.kr>
2025-02-21 15:08:41 +0900

    /calendar로 리다이렉트

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/calendar",
+        permanent: true, // Set `true` for 301 (permanent redirect), `false` for 302 (temporary)
+      },
+    ];
+  },
   images: {
     domains: ['res.cloudinary.com'],
   },
