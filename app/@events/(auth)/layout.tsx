5517b6a15cd52b279a7f69da6ae2ae483bb72958
정솔미 <solmi-@kookmin.ac.kr>
2025-02-21 09:41:31 +0900

    괄호 폴더 밖으로 뺌

--- app/@events/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function AuthLayout({ children }: {
-  readonly children: React.ReactNode;
-}) {
-  return (
-    <div className="flex flex-col">
-      {children}
-    </div>
-  );
-}
