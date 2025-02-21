fcaafe8af17a9455adb81cde496ffd4387000a7d
정솔미 <solmi-@kookmin.ac.kr>
2025-02-21 09:50:31 +0900

    suspense

--- app/ui/right-section.tsx
+++ app/ui/right-section.tsx
@@ -1,7 +1,11 @@
+import { Suspense } from "react";
+
 export default async function RightSection({children}: {children: React.ReactNode}) {
   return (
     <section className="relative w-auto h-[calc(100%-4rem)] z-30 overflow-x-auto">
-      {children}
+      <Suspense fallback={<div>Loading...</div>}>
+        {children}
+      </Suspense>
     </section>
   );
 }
