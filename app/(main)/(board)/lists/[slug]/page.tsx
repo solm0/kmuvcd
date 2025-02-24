a10a4203930a884088725731e95b5f84d80d3c02
정솔미 <solmi-@kookmin.ac.kr>
2025-02-24 23:37:05 +0900

    페이지 로딩이 너무 길어서 임시방편으로 Suspense

--- app/(main)/(board)/lists/[slug]/page.tsx
+++ app/(main)/(board)/lists/[slug]/page.tsx
@@ -1,7 +1,12 @@
 import SlugPage from "@/app/ui/slug-page";
+import { Suspense } from "react";
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
 
-  return <SlugPage slug={slug} />
+  return (
+    <Suspense fallback={<div className="w-full">Loading post...</div>}>
+      <SlugPage slug={slug} />
+    </Suspense>
+  )
 }
