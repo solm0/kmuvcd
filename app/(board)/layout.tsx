0959be023565e4143635389974030993ca2c5e14
정솔미 <solmi-@kookmin.ac.kr>
2025-03-11 15:33:09 +0100

    //

--- app/(board)/layout.tsx
+++ app/(board)/layout.tsx
@@ -1,26 +1,10 @@
-'use client'
+import BoardLayout from "../ui/board-layout";
 
-import { useSearchParams } from "next/navigation";
-import clsx from "clsx";
-import { useState, useEffect } from "react";
-
-export default function Layout({children}: {children: React.ReactNode}) {
-  const searchParams = useSearchParams();
-
-  const [isOpen, setIsOpen] = useState(false);
-
-  useEffect(() => {
-    if (searchParams.get("expand") === "true") {
-      setIsOpen(true);
-    } else {
-      setIsOpen(false);
-      // console.log('closed', isOpen)
-    }
-  }, [searchParams]);
-
+export default async function Layout({children}: {children: React.ReactNode}) {
+ 
   return(
-    <div className={clsx("w-full h-full flex flex-col", isOpen ? "bg-gray-200 hover:bg-white transition-colors duration-300" : "bg-white")}>
+    <BoardLayout>
       {children}
-    </div>
+    </BoardLayout>
   );
 }
---
+++ app/ui/board-layout.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useSearchParams } from "next/navigation";
+import clsx from "clsx";
+import { useState, useEffect } from "react";
+
+export default function BoardLayout({children}: {children: React.ReactNode}) {
+  const searchParams = useSearchParams();
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (searchParams.get("expand") === "true") {
+      setIsOpen(true);
+    } else {
+      setIsOpen(false);
+      // console.log('closed', isOpen)
+    }
+  }, [searchParams]);
+
+  return(
+    <div className={clsx("w-full h-full flex flex-col", isOpen ? "bg-gray-200 hover:bg-white transition-colors duration-300" : "bg-white")}>
+      {children}
+    </div>
+  );
+}
