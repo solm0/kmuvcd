3e5169d26b9c49c5b89f8e2723d4dd4ac89667d1
정솔미 <solmi-@kookmin.ac.kr>
2025-04-01 21:18:05 +0200

    ⨯ useSearchParams() should be wrapped in a suspense boundary at page "/404" 에러: 전 커밋에서 내가 Suspense안에있던 애들을 헤더로 뺀거임. 그래서 Suspense로 다시 감싸줬음.

--- app/ui/header-section.tsx
+++ app/ui/header-section.tsx
@@ -2,16 +2,19 @@ import DashboardButton from './header/dashboard-button';
 import DocsButton from './header/docs-button';
 import ViewButton from './header/view-button';
 import CategoryButton from './header/category-button';
+import { Suspense } from 'react';
 
 export default async function HeaderSection() {
   return (
     <section
       className="fixed w-screen h-16 flex z-20 p-4 gap-4"
     >
-      <DocsButton />
-      <ViewButton />
-      <CategoryButton />
-      <DashboardButton />
+      <Suspense>
+        <DocsButton />
+        <ViewButton />
+        <CategoryButton />
+        <DashboardButton />
+      </Suspense>
     </section>
   );
 }
