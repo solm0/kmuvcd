d61a3be64e292230c6961e13d3ce4d29ca1f8f5d
정솔미 <solmi-@kookmin.ac.kr>
2025-04-02 23:21:00 +0200

    디테일페이지 md sm, open일때 sm처럼

--- app/ui/board/detail-window.tsx
+++ app/ui/board/detail-window.tsx
@@ -1,7 +1,10 @@
+'use client'
+
 import { usePathname, useSearchParams } from "next/navigation";
 import { X } from "lucide-react";
 import Link from "next/link";
 import clsx from "clsx";
+import { useState, useEffect } from "react";
 
 export default function DetailWindow({ children }: { children: React.ReactNode }) {
   const searchParams = useSearchParams();
@@ -12,8 +15,24 @@ export default function DetailWindow({ children }: { children: React.ReactNode }
     return `/?${searchParams}`;
   }
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (searchParams.get("expand") === "true") {
+      setIsOpen(true);
+    } else {
+      setIsOpen(false);
+      // console.log('closed', isOpen)
+    }
+  }, [searchParams]);
+
   return (
-      <div className={clsx("absolute right-0 top-0 bg-white w-1/2 h-full z-10 overflow-auto", {"hidden": isCleanPath})}>
+      <div className={clsx(
+        "absolute right-0 top-0 bg-white/20 backdrop-blur-md border border-gray-500 w-full h-full md:w-1/2 md:h-auto max-h-full z-10 overflow-x-auto",
+        isOpen && "md:w-[100%] md:h-[100%]",
+        isCleanPath && "hidden"
+        )}
+      >
         <div className="h-12 p-4 flex absolute w-full items-center">
           <Link
             href={generateHref(searchParams.toString())}
