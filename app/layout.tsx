8ed6b7eeff78fa2cd66e7e5824fbf6ed2c83b399
정솔미 <solmi-@kookmin.ac.kr>
2025-04-10 17:50:16 +0200

    || null, page.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
               {children}
             </RightSection>
             
-            {modal}
+            {modal || null}
           </section>
         </div>
       </body>
