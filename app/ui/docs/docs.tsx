9e2a9524467f5b4be4fa2ad7e5b97b117feb063f
정솔미 <solmi-@kookmin.ac.kr>
2025-05-15 20:24:28 +0200

    .

--- app/ui/docs/docs.tsx
+++ app/ui/docs/docs.tsx
@@ -6,12 +6,12 @@ export default function Docs({children}: {children: React.ReactNode}) {
 
   return (
     <nav className="w-full h-auto md:h-full md:w-auto md:flex">
-      <div className="relative flex h-full w-full md:h-full transition-[width, height, colors] duration-300 z-10">
-          <div className="w-auto border-r border-slate-500">
+      <div className="h-full w-full md:h-full transition-[width, height, colors] duration-300 z-10">
+          <div className="w-auto fixed">
             <DocIndex />
           </div>
           <div
-            className="flex-1 h-full overflow-y-auto overflow-x-hidden [&::-webkit-scrollbar]:w-4  [&::-webkit-scrollbar-thumb]:bg-gray-100"
+            className="border-l border-slate-500 ml-56 h-full overflow-y-auto overflow-x-hidden [&::-webkit-scrollbar]:w-4  [&::-webkit-scrollbar-thumb]:bg-gray-100"
           >
             {children}
           </div> 
--- app/ui/header/docs-button.tsx
+++ app/ui/header/docs-button.tsx
@@ -1,12 +1,20 @@
+'use client'
+
 import Link from "next/link"
+import clsx from "clsx"
+import { usePathname } from "next/navigation"
 
 export default function DocsButton() {
+  const pathname = usePathname();
 
   return (
-      <div className="fixed w-28 h-8 transition-colors duration-300 hover:text-gray-400">
+      <div className="fixed w-28 h-8 transition-colors duration-300">
         <Link
           href="/"
-          className='h-8 flex items-center gap-2 text-sm break-keep'
+          className={clsx(
+            'h-8 flex items-center gap-2 text-sm break-keep hover:text-gray-400',
+            {"text-gray-400": pathname === '/'}
+          )}
         >
           국민대학교 시각디자인학과
         </Link>
